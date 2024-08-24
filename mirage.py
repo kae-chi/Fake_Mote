747f7d45e9bf27d56cba373c5e15813693cfbb22
kae-chi <kechi@bu.edu>
2024-08-24 16:36:11 -0700

    tried resolving ack packet

--- mirage.py
+++ mirage.py
@@ -2,24 +2,8 @@ import time
 import threading
 import scapy.all as scapy
 import random
-import sys
+
 import struct
-import contextlib 
-import os
-
-
-
-def suppress_output():
-    with open(os.devnull, 'w') as fnull:
-        original_stdout = sys.stdout
-        original_stderr = sys.stderr
-        sys.stdout = fnull
-        sys.stderr = fnull
-        try:
-            yield
-        finally:
-            sys.stdout = original_stdout
-            sys.stderr = original_stderr
 
 
 class Network:
@@ -27,7 +11,7 @@ class Network:
         source_ip = None
         dest_ip = None
         self.dest_port = 8888
-        self.src_port = scapy.RandShort()
+        self.src_port = 12345
         self.interface = "en0"
         self.ip_filters = []
         self.sensors = {}
@@ -43,9 +27,7 @@ class Network:
         ip_layer = scapy.IP(src=self.source_ip, dst=self.dest_ip)
         udp_layer = scapy.UDP(dport=self.dest_port, sport=self.src_port)
         payload = scapy.Raw(load=message)
-        with open(os.devnull, 'w') as fnull:
-            with contextlib.redirect_stdout(fnull), contextlib.redirect_stderr(fnull):
-                scapy.send(ip_layer / udp_layer / payload)
+        scapy.send(x=(ip_layer / udp_layer / payload), verbose=False)
 
     def add_ip_filters(self, input):
         for e in input:
@@ -67,37 +49,47 @@ class Network:
                     if len(data) != 2:
                         pass
                     else:
+               
                         # heartbeat
                         if data == bytes([int(100), 235]):
+
                             pass
                         # reset sensors command
                         elif data == bytes([0, 44]):
                             print("resetting!")
                             self.sensors = {}
+                            self.actuators = {}
                         else:
                             print("data recieved, now parsing")
+                    
                             pin_num = data[0]
+                 
                             config = data[1]
+                         
 
                             is_an_actuator = config & 0b10000000 == 0b10000000
                             interface_type_number = config & 0b00111111
 
                             if is_an_actuator:
+
                                 actuator_state = config & 0b01000000 == 0b01000000
+
                                 # pin num: [actuator state, interface number]
                                 self.actuators[pin_num] = [
-                                    boolean(actuator_state),
+                                    actuator_state,
                                     interface_type_number,
                                 ]
                                 print(
                                     f"pin {pin_num} is an actuator with the state {actuator_state} with the interface of {interface_type_number}"
-                                ) 
-                                packet = bytearray(4)
-                                
-                                # first byte is the pin
-                                packet[0] = pin_num
-                                # translating the random fuzz to bytes and filling in the remaining 4 bytes
-                                packet[ 1 : 3] = struct.pack(">f", random.random())
+                                )
+
+                                ack_pin = 100 + pin_num
+                                ack_packet = bytes([ack_pin, config])
+                                print(ack_pin)
+                                print(config)
+                                self.send_packet(ack_packet)
+                                print(ack_packet)
+
                             else:
                                 print(
                                     f"pin {pin_num} is a sensor with the interface {interface_type_number}"
@@ -120,7 +112,9 @@ class Network:
                     # first byte is the pin
                     packet[index] = k
                     # translating the random fuzz to bytes and filling in the remaining 4 bytes
-                    packet[index + 1 : index + 5] = struct.pack(">f", random.random())
+                    packet[index + 1 : index + 5] = struct.pack(
+                        ">f", random.randint(0, 100)
+                    )
 
                 # send packet at the end
                 self.send_packet(packet)
@@ -130,13 +124,6 @@ class Network:
         if self.actuators[number][1] == False:
             # some mathemetical operation occurs to the censors
             pass
-
-
-# helpers
-def boolean(bit):
-    if bit == 1:
-        return True
-    return False
 
 
 def send_heartbeat(network, message):
@@ -153,7 +140,7 @@ if __name__ == "__main__":
     fakemote1.set_dest_ip("127.0.0.1")
     fakemote1.add_ip_filters(["192.168.1.101"])
 
-    thread1 = threading.Thread(target=send_heartbeat, args=(fakemote1, "hello!"))
+    thread1 = threading.Thread(target=send_heartbeat, args=(fakemote1, bytes([0])))
     thread2 = threading.Thread(target=fakemote1.start_sniffing)
     thread3 = threading.Thread(target=fakemote1.fuzzing)
 
